e107b6e8ed406de3539c0fc21325d4cd8dd604b3
Joakim Bakke Hellum <joakim.bakke.hellum@innovasjonnorge.no>
2020-06-30 17:47:06 +0200

    avoid using git submodules when running acceptance tests

--- internal/git/data_source_git_repository_test.go
+++ internal/git/data_source_git_repository_test.go
@@ -2,19 +2,29 @@ package git
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
 	"path/filepath"
 	"testing"
 
+	"github.com/go-git/go-git/v5"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
 )
 
 func TestAccDataSourceGitRepository_path(t *testing.T) {
-	cwd, err := os.Getwd()
+	url := "https://github.com/innovationnorway/terraform-git-module-acctest.git"
+	dir, err := ioutil.TempDir("", "acctest-*")
 	if err != nil {
 		t.Fatal(err)
 	}
-	path := filepath.ToSlash(filepath.Join(cwd, "testdata", "terraform-git-module-acctest"))
+	_, err = git.PlainClone(dir, false, &git.CloneOptions{
+		URL: url,
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(dir)
+	path := filepath.ToSlash(dir)
 
 	resource.Test(t, resource.TestCase{
 		Providers: testAccProviders,
@@ -22,6 +32,7 @@ func TestAccDataSourceGitRepository_path(t *testing.T) {
 			{
 				Config: testAccDataSourceGitRepositoryPathConfig(path),
 				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("data.git_repository.test", "url", "https://github.com/innovationnorway/terraform-git-module-acctest.git"),
 					resource.TestCheckResourceAttr("data.git_repository.test", "branch", "master"),
 					resource.TestCheckResourceAttr("data.git_repository.test", "commit_sha", "750e948880ebe167eba524dded790e8b9a79d01d"),
 				),
@@ -31,20 +42,16 @@ func TestAccDataSourceGitRepository_path(t *testing.T) {
 }
 
 func TestAccDataSourceGitRepository_branch(t *testing.T) {
-	cwd, err := os.Getwd()
-	if err != nil {
-		t.Fatal(err)
-	}
-	path := filepath.ToSlash(filepath.Join(cwd, "testdata", "terraform-git-module-acctest"))
-
+	url := "https://github.com/innovationnorway/terraform-git-module-acctest.git"
 	branch := "master"
 
 	resource.Test(t, resource.TestCase{
 		Providers: testAccProviders,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccDataSourceGitRepositoryBranchConfig(path, branch),
+				Config: testAccDataSourceGitRepositoryBranchConfig(url, branch),
 				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("data.git_repository.test", "url", "https://github.com/innovationnorway/terraform-git-module-acctest.git"),
 					resource.TestCheckResourceAttr("data.git_repository.test", "branch", "master"),
 					resource.TestCheckResourceAttr("data.git_repository.test", "commit_sha", "750e948880ebe167eba524dded790e8b9a79d01d"),
 				),
@@ -54,29 +61,6 @@ func TestAccDataSourceGitRepository_branch(t *testing.T) {
 }
 
 func TestAccDataSourceGitRepository_tag(t *testing.T) {
-	cwd, err := os.Getwd()
-	if err != nil {
-		t.Fatal(err)
-	}
-	path := filepath.ToSlash(filepath.Join(cwd, "testdata", "terraform-git-module-acctest"))
-
-	tag := "v0.1.0"
-
-	resource.Test(t, resource.TestCase{
-		Providers: testAccProviders,
-		Steps: []resource.TestStep{
-			{
-				Config: testAccDataSourceGitRepositoryTagConfig(path, tag),
-				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr("data.git_repository.test", "tag", "v0.1.0"),
-					resource.TestCheckResourceAttr("data.git_repository.test", "commit_sha", "750e948880ebe167eba524dded790e8b9a79d01d"),
-				),
-			},
-		},
-	})
-}
-
-func TestAccDataSourceGitRepository_HTTPURL(t *testing.T) {
 	url := "https://github.com/innovationnorway/terraform-git-module-acctest.git"
 	tag := "v0.1.0"
 
@@ -84,10 +68,10 @@ func TestAccDataSourceGitRepository_HTTPURL(t *testing.T) {
 		Providers: testAccProviders,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccDataSourceGitRepositoryURLConfig(url, tag),
+				Config: testAccDataSourceGitRepositoryTagConfig(url, tag),
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr("data.git_repository.test", "url", url),
-					resource.TestCheckResourceAttr("data.git_repository.test", "tag", tag),
+					resource.TestCheckResourceAttr("data.git_repository.test", "url", "https://github.com/innovationnorway/terraform-git-module-acctest.git"),
+					resource.TestCheckResourceAttr("data.git_repository.test", "tag", "v0.1.0"),
 					resource.TestCheckResourceAttr("data.git_repository.test", "commit_sha", "750e948880ebe167eba524dded790e8b9a79d01d"),
 				),
 			},
@@ -103,25 +87,16 @@ data git_repository "test" {
 `, path)
 }
 
-func testAccDataSourceGitRepositoryBranchConfig(path string, branch string) string {
+func testAccDataSourceGitRepositoryBranchConfig(url string, branch string) string {
 	return fmt.Sprintf(`
 data git_repository "test" {
-  path   = "%s"
+  url   = "%s"
   branch = "%s" 
 }
-`, path, branch)
+`, url, branch)
 }
 
-func testAccDataSourceGitRepositoryTagConfig(path string, tag string) string {
-	return fmt.Sprintf(`
-data git_repository "test" {
-  path = "%s"
-  tag  = "%s" 
-}
-`, path, tag)
-}
-
-func testAccDataSourceGitRepositoryURLConfig(url string, tag string) string {
+func testAccDataSourceGitRepositoryTagConfig(url string, tag string) string {
 	return fmt.Sprintf(`
 data git_repository "test" {
   url = "%s"
